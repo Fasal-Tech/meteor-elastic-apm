795d25f64d677159d05be8074d7f7ec288e04cba
Kesha Shah <keshashah@wolkus.com>
2023-08-01 16:37:27 +0530

    changing package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 Package.describe({
-  name: 'kschingiz:meteor-elastic-apm',
+  name: 'psrwolkus:meteor-elastic-apm',
   version: '2.5.1',
   // Brief, one-line summary of the package.
   summary: 'Performance monitoring for Meteor based on Elastic APM',
@@ -19,8 +19,8 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('2.4');
 
-  api.use('kschingiz:meteor-measured@1.0.3');
-  api.imply('kschingiz:meteor-measured');
+  api.use('psrwolkus:meteor-measured@1.0.3');
+  api.imply('psrwolkus:meteor-measured');
 
   api.use([
     'ecmascript',
@@ -38,6 +38,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('kschingiz:meteor-elastic-apm');
+  api.use('psrwolkus:meteor-elastic-apm');
   api.mainModule('meteor-elastic-apm-tests.js');
 });
